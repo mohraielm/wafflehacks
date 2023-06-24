2d2b686cb9907a3e4866d3d4dd42b173ee2f66ad
Mohraiel Matta <mohraielmatta@gmail.com>
2023-06-23 17:03:26 -0700

    description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <nav className="flex justify-between items-center w-full mb-10 pt-3">
             <img src={logo} alt="AItinerary_logo"
             className="w-28 object-contain"/>
-
+{/* this creates the github button */}
             <button
             type="button"
             onClick={() => window.open('https://github.com/mohraielm/wafflehacks')}
@@ -14,12 +14,17 @@ const Hero = () => {
                 Github
             </button>
         </nav>
+    {/* header title */}
         <h1 className="head_text">
             Create your next itinerary with <br 
             className="max-md:hidden"/>
             <span className="green_gradient"> AI-tinerary </span>
 
         </h1>
+        <h2 className="desc">
+            Travelling soon? Plan your next itinerary with your AI assistant!
+
+        </h2>
         </header>
   )
 }
