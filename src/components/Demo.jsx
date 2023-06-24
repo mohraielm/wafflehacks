c23259f58d87691332ff1c7160b133661c7e9cd1
Mohraiel Matta <mohraielmatta@gmail.com>
2023-06-23 17:34:02 -0700

    kayuun help

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useState,useEffect } from "react"
+import {copy,linkIcon,loader,tick} from '../assets/assets';
 
 const Demo = () => {
   return (
-    <div>Demo</div>
+    <section className="mt-16 w-full max-w-xl">
+      {/*Search*/}
+      <div className="flex flex-col w-full gap-2">
+
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}
+          >
+            <img
+              src={linkIcon}
+              alt="link_icon"
+              className="absolute left-0 my-2 ml-3 w-5"
+              />
+
+              <input
+              type="text"
+              placeholder="Enter a destination"
+              value=""
+              onChange={()=>{}}
+              required
+              className="text_input peer"
+              />
+
+              <button
+              type="submit"
+              className="submit_btn
+               peer-focus:border-gray-700 
+               peer-forcus:text-gray-700">
+
+              </button>
+        </form>
+      </div>
+
+    </section>
   )
 }
 
