af0ee2e69c82c2dd640c711bc1f42937ebb24385
Mohraiel Matta <mohraielmatta@gmail.com>
2023-06-23 17:07:12 -0700

    TRANSPARENT LOGO

--- src/assets/assets/index.js
+++ src/assets/assets/index.js
@@ -1,7 +1,7 @@
 import linkIcon from './link.svg'
 import loader from './loader.svg'
 import copy from './copy.svg'
-import logo from './logo1.png'
+import logo from './logo.png'
 import tick from './tick.svg'
 
 export {
