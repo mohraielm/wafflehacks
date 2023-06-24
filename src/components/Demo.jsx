8ee47006fd119d07ea82fef21bf2b5287b5d16ed
Mohraiel Matta <mohraielmatta@gmail.com>
2023-06-23 17:51:15 -0700

    updated again

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -31,7 +31,7 @@ const Demo = () => {
               className="submit_btn
                peer-focus:border-gray-700 
                peer-forcus:text-gray-700">
-                ↩
+                 <p>💼</p>
               </button>
         </form>
       </div>
